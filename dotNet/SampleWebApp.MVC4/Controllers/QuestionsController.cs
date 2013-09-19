04ec1b74c8b270d77ef339e9de98776ee921600d
synhershko <itamar@code972.com>
2013-09-19 21:43:54 +0300

    Going for a StackOverflow example

---
+++ dotNet/SampleWebApp.MVC4/Controllers/QuestionsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Dynamic;
+using System.Web.Mvc;
+using SampleWebApp.Common.ViewModels;
+
+namespace SampleWebApp.MVC4.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Ask()
+        {
+            dynamic viewModel = new ExpandoObject();
+            viewModel.User = new UserViewModel(User) { Id = User.Identity.Name, Name = User.Identity.Name };
+            viewModel.Question = new QuestionInputModel();
+            return View(viewModel);
+        }
+
+        [HttpPost] // Authorize
+        public ActionResult Ask(QuestionInputModel inputModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var q = inputModel.ToQuestion();
+                q.CreatedBy = "users/1"; // Just a stupid default because we haven't implemented log-in
+
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
+            dynamic viewModel = new ExpandoObject();
+            viewModel.User = new UserViewModel(User) { Id = User.Identity.Name, Name = User.Identity.Name };
+            viewModel.Question = inputModel;
+            return View(viewModel);
+        }
+    }
+}
