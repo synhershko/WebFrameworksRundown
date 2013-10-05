3b9058108b987d709abbb7e1e76aaefa1e144984
synhershko <itamar@code972.com>
2013-10-05 23:32:46 +0300

    Enable Nancy diagnostics

--- dotNet/SampleWebApp.Nancy/Bootstrapper.cs
+++ dotNet/SampleWebApp.Nancy/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using Nancy.Diagnostics;
 
 namespace SampleWebApp.Nancy
 {
@@ -9,5 +10,16 @@ namespace SampleWebApp.Nancy
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ApplicationStartup(global::Nancy.TinyIoc.TinyIoCContainer container, global::Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            // Can now use this.Context.Trace.TraceLog.WriteLog(s => s.AppendLine(“Root path was called”)); from route handlers
+            StaticConfiguration.EnableRequestTracing = true;
+        }
+
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration { Password = "12345" }; }
+        }
     }
 }
